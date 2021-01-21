1b23d6b1f1eeca8fac6232dfcca5050bf9d7d75b
Ryan Cheng <ryanc552@gmail.com>
2021-01-21 16:24:16 -0500

    cah_product

--- amazonscraper.py
+++ amazonscraper.py
@@ -1,9 +1,7 @@
 from requests_html import HTMLSession
 import pandas as pd
 
-urls = ['https://www.amazon.co.uk/Samsung-TU7100-Smart-CARBON-SILVER/dp/B086WGXQFZ/ref=sr_1_2?dchild=1&keywords=tv&qid=1601927782&refinements=p_n_size_browse-bin%3A9591881031&rnid=161398031&s=home-theater&sr=1-2',
-          'https://www.amazon.co.uk/Samsung-TU7100-HDR-Smart-Tizen/dp/B086T39T5D/ref=sr_1_3?dchild=1&keywords=tv&qid=1601927782&refinements=p_n_size_browse-bin%3A9591881031&rnid=161398031&s=home-theater&sr=1-3',
-          'https://www.amazon.co.uk/LG-75UN81006LB-Freeview-Freesat-built/dp/B0853M2XB1/ref=sr_1_7?dchild=1&keywords=tv&qid=1601927782&refinements=p_n_size_browse-bin%3A9591881031&rnid=161398031&s=home-theater&sr=1-7']
+url = 'https://www.amazon.com/Cardinal-Moderate-Absorbency-Protective-Underwear/dp/B076X4GRPT/ref=sr_1_1_sspa?dchild=1&keywords=cardinal+health&qid=1611254997&sr=8-1-spons&psc=1&smid=AE309QMZ3IOFX&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzN0pUVzFQQ0c2RkJTJmVuY3J5cHRlZElkPUEwMDM5MzEzTTlKTk9PV1NLNUpGJmVuY3J5cHRlZEFkSWQ9QTA5MzgxODcxME0wNFdLM1BST08xJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 
 def getPrice(url):
     s = HTMLSession()
@@ -12,7 +10,9 @@ def getPrice(url):
     try:
         product = {
             'title': r.html.xpath('//*[@id="productTitle"]', first=True).text,
-            'price': r.html.xpath('//*[@id="priceblock_ourprice"]', first=True).text
+            'price': r.html.xpath('//*[@id="priceblock_ourprice"]', first=True).text,
+            'shipping': r.html.xpath('//*[@id="price-shipping-message"]/b', first = True).text,
+            'quantity': r.html.xpath('//*[@id="variation_item_package_quantity"]/div/span', first = True).text
         }
         print(product)
     except:
@@ -23,6 +23,8 @@ def getPrice(url):
         print(product)
     return product
 
+getPrice(url)
+
 tvprices = []
 for url in urls:
     tvprices.append(getPrice(url))
@@ -31,3 +33,4 @@ print(len(tvprices))
 
 pricesdf = pd.DataFrame(tvprices)
 pricesdf.to_excel('tvprices.xlsx', index=False)
+
