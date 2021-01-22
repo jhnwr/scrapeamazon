e6f05cfeff405dd966cac249fa3a8ba315575bee
Ryan Cheng <ryanc552@gmail.com>
2021-01-22 12:39:13 -0500

    added methods for spec table

--- amazonscraper.py
+++ amazonscraper.py
@@ -1,26 +1,14 @@
-from requests_html import HTMLSession
-import pandas as pd
+from my_methods import getPrice, get_product_spec
 
-def getPrice(url):
-    s = HTMLSession()
-    r = s.get(url)
-    r.html.render(sleep=1)
-    product = {}
-    product['title'] = r.html.xpath('//*[@id="productTitle"]', first=True).text
-    product['price'] = r.html.xpath('//*[@id="priceblock_ourprice"]', first=True).text
-    product['shipping'] = r.html.xpath('//*[@id="price-shipping-message"]/b', first=True).text
-    product['brand_name'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[1]/td', first = True).text
-    product['ean'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[2]/td', first=True).text
-    product['included_componenets'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[3]/td', first=True).text
-    product['model_number'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[4]/td', first=True).text
-    product['num_items'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[5]/td', first=True).text
-    product['part_num'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[6]/td', first=True).text
-    product['upc'] = r.html.xpath('//*[@id="product-specification-table"]/tbody/tr[10]/td', first=True).text
-    # product[''] = r.html.xpath('', first=True).text
-    
-    print(product)
-    return product
+url1 = r'https://www.amazon.com/Medline-Washable-Underpads-Incontinence-Training/dp/B07P83ZDV4'
+url2 = r'https://www.amazon.com/FitRight-Diapers-Disposable-Incontinence-Absorbency/dp/B00KHUMB1E'
+url3 = r'https://www.amazon.com/FitRight-Heavyweight-Cleansing-Unscented-Incontinence/dp/B01KGH1T92'
 
-url = 'https://www.amazon.com/Medline-Washable-Underpads-Incontinence-Training/dp/B07P83ZDV4?ref_=ast_sto_dp'
+# url_list = [url1, url2, url3]
+# url_list = [url1]
+spec_list = []
 
-getPrice(url)
+product = get_product_spec(url1)
+
+product_list = [product]
+
