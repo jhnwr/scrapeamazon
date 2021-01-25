65919f18037a09fdfbf843a8f2a6635c9061a8af
Ryan Cheng <ryanc552@gmail.com>
2021-01-25 15:53:11 -0500

    updated

--- test.py
+++ test.py
@@ -10,28 +10,5 @@ search_list
 
 search_url = search_list[1]
 
-s = HTMLSession()
-r = s.get(search_url)
-r.html.render(sleep=1)
-
-xpath = r'//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
-
-deeper_xpath = r'//*[@id="search"]/div[1]/div[2]/div/span[3]/div/div/div/span/div/div/div/div/span/a'
-results_page = r.html.xpath(deeper_xpath)
-
-prod_url_list = []
-for node in results_page:
-    
-    url_end = node.attrs['href']
-    url_base = r'https://www.amazon.com'
-    prod_url = url_base + url_end
-    prod_url_list.append(prod_url)
-
-
-len(prod_url_list)
-
-for url in prod_url:
-    print(url)
-    
 
 #need to either remove sponsored pages, or scrape seller from the page
