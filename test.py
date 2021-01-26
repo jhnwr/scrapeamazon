7b5e3b107e154b50c5daa5ab63b466d861e97ac3
Ryan Cheng <ryanc552@gmail.com>
2021-01-26 15:26:29 -0500

    something

--- test.py
+++ test.py
@@ -1,35 +1,16 @@
 from requests_html import HTMLSession
-from my_methods import get_search_urls, get_prod_urls, scrape_info
+from my_methods import get_search_urls, get_prod_urls, scrape_info, get_items_from_list
 from time import sleep, time
 import pandas as pd
-start_time = time()
+# TODO create method to get a url list
+# This will also need to be a slow cralwer so as not to get detected
 
-# define fn that gets all product_urls from a search_url
+url1 = r'https://www.amazon.com/Medline-Remedy-Unscented-Olivamine-Repair/dp/B008107QBM/ref=sr_1_8?dchild=1&keywords=medline&qid=1611687162&s=hpc&sr=1-8&th=1'
+url2 = r'https://www.amazon.com/Medline-Remedy-Phytoplex-Nourishing-Cream/dp/B00C0LPCZ0/ref=psdc_11060681_t1_B008107QBM'
+url_spec_box = r'https://www.amazon.com/Medline-Disposable-incontinence-Furniture-Protection/dp/B075HKJZ2R/ref=sr_1_2?dchild=1&keywords=medline&qid=1611690950&s=hpc&sr=1-2'
+prod = scrape_info(url_spec_box)
 
-dept_url = r'https://www.amazon.com/s?k=medline&i=hpc&rh=n%3A3760901&page='
-search_list = get_search_urls(dept_url)
+path_out = r'/Users/ryancheng/Projects/scrapeamazon/df_results_TEST.csv'
 
-prod_info = []
-for search_url in search_list[:1]:
-    
-    prod_urls = get_prod_urls(search_url)
-    for prod_url in prod_urls:
-        print('Scraping URL: ', prod_url)
-        prod_info.append(scrape_info(prod_url))
-        sleep(1)
-
-i = 0
-df_list = []
-for prod in prod_info:
-    df = pd.DataFrame(prod, index = [i])
-    df_list.append(df)
-    i+=1
-    
-df = pd.concat(df_list)
-
-path_out = r'/Users/ryancheng/Projects/scrapeamazon/df_results.csv'
-
+df = pd.DataFrame(prod, index = [0])
 df.to_csv(path_out)
-
-exec_time = time() - start_time
-print('Scraping completed in: ', exec_time, ' seconds.')
